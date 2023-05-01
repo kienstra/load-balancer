6402c38e12e8e9816a3223ed9bff1c8f27a4b60e
Ryan Kienstra <kienstraryan@gmail.com>
2023-05-01 15:06:15 -0600

    Create project with lein new app

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to load-balancer
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/load_balancer/core_test.clj
@@ -0,0 +1,7 @@
+(ns load-balancer.core-test
+  (:require [clojure.test :refer :all]
+            [load-balancer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
