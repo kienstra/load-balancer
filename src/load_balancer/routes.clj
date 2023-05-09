a9b00b06d16c5ca8b0f32b119c16cece88103755
Ryan Kienstra <kienstraryan@gmail.com>
2023-05-08 18:14:15 -0600

    Make routes.clj lines less long

--- src/load_balancer/routes.clj
+++ src/load_balancer/routes.clj
@@ -9,7 +9,17 @@
   (-> (GET "/" request (do
                          (log-request request)
                          (:body (deref (client/request (into
-                                                        (select-keys request [:method :timeout :connect-timeout :idle-timeout :query-params :as :form-params :client :body :basic-auth :user-agent])
+                                                        (select-keys request [:method
+                                                                              :timeout
+                                                                              :connect-timeout
+                                                                              :idle-timeout
+                                                                              :query-params
+                                                                              :as
+                                                                              :form-params
+                                                                              :client
+                                                                              :body
+                                                                              :basic-auth
+                                                                              :user-agent])
                                                         {:url (url!) :headers {"accept" (get (:headers request) "accept")}}))))))
       wrap-reload
       wrap-params))
