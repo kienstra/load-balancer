337a4b7129fb4209e8459a73ea3542af31b75baa
Ryan Kienstra <kienstraryan@gmail.com>
2023-05-14 18:51:42 -0600

    Make the polling interval a command line argument

--- src/load_balancer/core.clj
+++ src/load_balancer/core.clj
@@ -10,7 +10,12 @@
     :default 80
     :id :port
     :parse-fn #(Integer/parseInt %)
-    :validate [#(< 0 % 50000) "The port should be between 0 and 50000"]]])
+    :validate [#(< 0 % 50000) "The port should be between 0 and 50000"]]
+   ["-i" "--interval INTERVAL" "Polling interval"
+    :default 10000
+    :id :polling-interval
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 100000) "The interval should be between 0 and 100000"]]])
 
 (defn -main [& args]
   (let [options (:options (parse-opts args cli-options))]
@@ -18,4 +23,4 @@
     (let [ports (be-ports 10)]
       (dorun (for [port ports]
                (run-server (be-app) {:port port})))
-      (init! ports 10000))))
+      (init! ports (:polling-interval options)))))
